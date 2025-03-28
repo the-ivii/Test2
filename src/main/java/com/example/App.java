2c22fc0c10b6a14861b7f028c82033ab492684c0
Divyanshi <divyanshitalreja12@gmail.com>
2025-03-29 04:12:53 +0530

    Added more to test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,5 +3,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Divyanshi World!");
+        System.out.println(2+2);
+        System.out.println("The anser should be goooddd!");
     }
 }
